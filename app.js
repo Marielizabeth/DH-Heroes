a6bebe3499efa39f02c675d285760538f9ec0aec
Marielizabeth <marielaerobles@gmail.com>
2021-06-23 02:06:59 -0300

    Trabajo terminado

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require('express'); 
+const app = express();
+const port = 3030;
+const path = require('path')
+
+
+app.listen(3030, () => console.log('Servidor ejecutandose en http://localhost:' + port))
+
+app.use(express.static('public'));
+
+app.get('/',(req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/index.html'))
+})
+
+app.get('/babbage',(req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/babbage.html'))
+})
+
+app.get('/berners-lee',(req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/berners-lee.html'))
+})
+
+app.get('/clarke',(req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/clarke.html'))
+})
+
+app.get('/hamilton',(req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/hamilton.html'))
+})
+
+app.get('/hopper',(req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/hopper.html'))
+})
+
+app.get('/lovelace',(req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/lovelace.html'))
+})
+
+app.get('/turing',(req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/turing.html'))
+})
